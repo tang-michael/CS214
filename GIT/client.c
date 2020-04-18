7c14583815a0d8615c09c4a037364f1ff53d8fb8
tang-michael <43308145+tang-michael@users.noreply.github.com>
2020-04-17 21:17:16 -0400

    Add files via upload
    Updated client

--- GIT/client.c
+++ GIT/client.c
@@ -1,77 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <string.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <ctype.h>
 
-#include <string.h>
+#include "util.h"
+
 
 int main(int argc, char *argv[]) {
    int sockfd, portno, n;
    struct sockaddr_in serv_addr;
    struct hostent *server;
+   char buffer[255];
 
-   char buffer[256];
-
-   if (argc < 3) {
-      fprintf(stderr,"usage %s hostname port\n", argv[0]);
-      exit(0);
-   }
-
-   portno = atoi(argv[2]);
-
-   /* Create a socket point */
-   sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-   if (sockfd < 0) {
-      perror("ERROR opening socket");
-      exit(1);
-   }
-
-   server = gethostbyname(argv[1]);
-
-   if (server == NULL) {
-      fprintf(stderr,"ERROR, no such host\n");
-      exit(0);
-   }
-
-   bzero((char *) &serv_addr, sizeof(serv_addr));
-   serv_addr.sin_family = AF_INET;
-   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-   serv_addr.sin_port = htons(portno);
-
-   /* Now connect to the server */
-   if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-      perror("ERROR connecting");
-      exit(1);
-   }
-
-   /* Now ask for a message from the user, this message
-      * will be read by server
-   */
-
-   printf("Please enter the message: ");
-   bzero(buffer,256);
-   fgets(buffer,255,stdin);
-
-   /* Send message to the server */
-   n = write(sockfd, buffer, strlen(buffer));
-
-   if (n < 0) {
-      perror("ERROR writing to socket");
-      exit(1);
-   }
-
-   /* Now read server response */
-   bzero(buffer,256);
-   n = read(sockfd, buffer, 255);
-
-   if (n < 0) {
-      perror("ERROR reading from socket");
-      exit(1);
-   }
-
-   printf("%s\n",buffer);
+   if(strcmp(argv[1], "configure") == 0){
+    int file = open(".Configure", O_WRONLY | O_CREAT | O_TRUNC, 00600);
+    int length1 = strlen(argv[2]);
+    int length2 = strlen(argv[3]);
+    write(file,argv[2], length1);
+    write(file," ", 1);
+    write(file,argv[3], length2);
+    /*
+    ADD ERROR CHECKING HERE
+    FOR WRITING FILES
+    */
+  }else if(strcmp(argv[1], "configure") != 0){
+    if(open(".Configure", O_RDONLY) < 0){
+      printf("Can't find file");
+      return 0;
+    }else{
+      char *contents = toString(".Configure");
+      int i = 0;
+      char word[255] = {0};
+      char *name;
+      while(i < strlen(contents)){
+        if(isspace(contents[i])){
+          name = word;
+          printf("%s\n", name);
+          memset(word,0,sizeof(word));
+          i++;
+          continue;
+        }
+        strncat(word, &contents[i], 1);
+        i++;
+      }
+        printf("name = %s\n",  name);
+        printf("portno = %s\n", word);
+    }
+  }
+    // portno = atoi(argv[2]);
+   // sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   //
+   // if (sockfd < 0) {
+   //    perror("ERROR opening socket");
+   //    exit(1);
+   // }
+   //
+   // server = gethostbyname(argv[1]);
+   //
+   // if (server == NULL) {
+   //    fprintf(stderr,"ERROR, no such host\n");
+   //    exit(0);
+   // }
+   //
+   // bzero((char *) &serv_addr, sizeof(serv_addr));
+   // serv_addr.sin_family = AF_INET;
+   // bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+   // serv_addr.sin_port = htons(portno);
+   //
+   // /* Now connect to the server */
+   // if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+   //    perror("ERROR connecting");
+   //    exit(1);
+   // }
+   //
+   // if (n < 0) {
+   //    perror("ERROR writing to socket");
+   //    exit(1);
+   // }
+   //
+   // /* Now read server response */
+   // bzero(buffer,256);
+   // n = read(sockfd, buffer, 255);
+   //
+   // if (n < 0) {
+   //    perror("ERROR reading from socket");
+   //    exit(1);
+   // }
+   //
+   // printf("%s\n",buffer);
    return 0;
 }
