c7d5131fd8385f00130a06ef1ed2325ab4c3a939
tang-michael <43308145+tang-michael@users.noreply.github.com>
2020-05-04 15:08:17 -0400

    Add files via upload

--- GITFinal/rollbackProject.c
+++ GITFinal/rollbackProject.c
@@ -5,9 +5,6 @@
 void clientRollback(char *projectName, int rollbackVersion)
 {
 
-
-	int versionNumber = 0;
-	int rollbackNumber = rollbackVersion;
 	//1
 	sendTo(SERVER, "rollback");
 	sendTo(SERVER, projectName);
@@ -22,9 +19,9 @@ void clientRollback(char *projectName, int rollbackVersion)
 	}
 
 	//2
-	int len = floor(log10(rollbackNumber)) + 1;
+	int len = floor(log10(rollbackVersion)) + 1;
 	char* versionStr = malloc(len + 1);
-	sprintf(versionStr, "%d",  rollbackNumber);
+	sprintf(versionStr, "%d",  rollbackVersion);
 	sendTo(SERVER, versionStr);
 	char* versionReturn = getFrom(SERVER);
 	if(strcmp(versionReturn, "WRONG") == 0)
@@ -35,28 +32,47 @@ void clientRollback(char *projectName, int rollbackVersion)
 		free(versionReturn);
 		return;
 	}
-
 	//3
-	get_version_number(projectName , &versionNumber);
-	save_version_number(projectName , &rollbackNumber);
-	//4
-	char* manifestName = generate_manifest_name(projectName, rollbackVersion);
-	printf("manifestName = %s\n", manifestName);
+	delete_direct_and_file(projectName);
+    mkdir(projectName, S_IRUSR | S_IWUSR | S_IXUSR);
+    char* manifestDirectory = malloc(strlen(projectName) + 5 );
+    sprintf(manifestDirectory, "%s/.git", projectName);
+    mkdir(manifestDirectory, S_IRUSR | S_IWUSR | S_IXUSR);
+
+	char *directTreeFile = malloc(strlen(projectName) + 21);
+	sprintf(directTreeFile, "%s/.git/directTree.txt", projectName );
+    receive_file(SERVER, directTreeFile);
+    mkdir_direct_tree(projectName, directTreeFile);
+    int versionNumber = rollbackVersion;
+    save_version_number(projectName , &versionNumber);
+	char* manifestName = generate_manifest_name(projectName, versionNumber);
 	receive_file(SERVER, manifestName);
-	char* directIndex = generate_direct_index_name(projectName, rollbackVersion);
-	printf("directIndex = %s\n", directIndex);
-	receive_file(SERVER, directIndex);
 
-	//5
-
-
-	free(versionStr);
-	free(versionReturn);
+	int i = 0;
+	int sizeManifest = 0;
+	manEntry** manifestEntry = readManifest(manifestName, &sizeManifest);
+	printf("manifestName = %s, sizeManifest %d\n",manifestName,sizeManifest);
+	for(i = 0; i < sizeManifest; i++)
+	{
+		if(strstr(manifestEntry[i]->name, ".manifest") == 0)
+		{
+			char* filename = truncate_version_number(manifestEntry[i]->name, versionNumber);
+			printf("filename = %s\n", filename);
+			receive_file(SERVER, filename);
+			free(filename);
+		}
+	}
 	free(existFlag);
+	free(versionStr);
+	free(manifestDirectory);
+	free(directTreeFile);
+	free(manifestName);
+	freeManifest(manifestEntry, &sizeManifest);
 }
 
 void serverRollback(int fd)
 {
+
 
 	//1
     char* projectName = getFrom(fd);
@@ -76,7 +92,6 @@ void serverRollback(int fd)
 	int versionNumber = 0;
 	get_version_number(projectName , &versionNumber);
 
-	printf("rollbackVersionNumber = %d, versionNumber = %d\n", rollbackVersionNumber, versionNumber);
 	if(rollbackVersionNumber > versionNumber)
 	{
 		sendTo(fd, "WRONG");
@@ -90,14 +105,29 @@ void serverRollback(int fd)
 	}
 
     //3
-	char* manifestName = generate_manifest_name(projectName, rollbackVersionNumber);
-	printf("manifestName = %s\n", manifestName);
-	send_file(fd, manifestName);
 	char* directIndex = generate_direct_index_name(projectName, rollbackVersionNumber);
 	printf("directIndex = %s\n", directIndex);
 	send_file(fd, directIndex);
+	char* manifestName = generate_manifest_name(projectName, rollbackVersionNumber);
+	printf("manifestName = %s\n", manifestName);
+	send_file(fd, manifestName);
 
+	//4
+	int i;
+	int sizeManifest = 0;
+	manEntry** manifestEntry = readManifest(manifestName, &sizeManifest);
 
+	for(i = 0; i < sizeManifest; i++)
+	{
+		if(strstr(manifestEntry[i]->name, ".manifest") == 0)
+		{
+			send_file(fd, manifestEntry[i]->name);
+		}
+	}
 
     free(projectName);
+    free(versionStr);
+    free(directIndex);
+    free(manifestName);
+    freeManifest(manifestEntry, &sizeManifest);
 }
--- GITFinal/upgradeProject.c
+++ GITFinal/upgradeProject.c
@@ -39,8 +39,48 @@ void clientUpgrade(char* projectName)
 		free(conflictFile);
 		return;
 	}
-	//4
-	send_file(SERVER, updateFile);
+
+	delete_direct_and_file(projectName);
+    mkdir(projectName, S_IRUSR | S_IWUSR | S_IXUSR);
+    char* manifestDirectory = malloc(strlen(projectName) + 5 );
+    sprintf(manifestDirectory, "%s/.git", projectName);
+    mkdir(manifestDirectory, S_IRUSR | S_IWUSR | S_IXUSR);
+
+	char *directTreeFile = malloc(strlen(projectName) + 21);
+	sprintf(directTreeFile, "%s/.git/directTree.txt", projectName );
+    receive_file(SERVER, directTreeFile);
+    mkdir_direct_tree(projectName, directTreeFile);
+	int versionNumber = 0;
+    char* versionStr = getFrom(SERVER);
+    versionNumber = atoi(versionStr);
+    save_version_number(projectName , &versionNumber);
+	char* manifestName = generate_manifest_name(projectName, versionNumber);
+	receive_file(SERVER, manifestName);
+
+	int i = 0;
+	int sizeManifest = 0;
+	manEntry** manifestEntry = readManifest(manifestName, &sizeManifest);
+	printf("manifestName = %s, sizeManifest %d\n",manifestName,sizeManifest);
+	for(i = 0; i < sizeManifest; i++)
+	{
+		if(strstr(manifestEntry[i]->name, ".manifest") == 0)
+		{
+			char* filename = truncate_version_number(manifestEntry[i]->name, versionNumber);
+			printf("filename = %s\n", filename);
+			receive_file(SERVER, filename);
+			free(filename);
+		}
+	}
+
+	free(updateFile);
+	free(conflictFile);
+	free(existFlag);
+	free(manifestDirectory);
+	free(directTreeFile);
+	free(versionStr);
+	free(manifestName);
+	freeManifest(manifestEntry, &sizeManifest);
+
 }
 void serverUpgrade(int fd)
 {
@@ -57,14 +97,42 @@ void serverUpgrade(int fd)
     	free(projectName);
     	return;
     }
-    //2
-    char *updateFile = malloc(strlen(projectName) + 14);
-    sprintf(updateFile, "%s/.git/.Update",projectName );
-    receive_file(fd, updateFile);
 
+    //1
+	char *directTreeFile = malloc(strlen(projectName) + 21);
+	sprintf(directTreeFile, "%s/.git/directTree.txt", projectName );
+	printf("directTreeFile = %s\n ",directTreeFile);
+	send_file(fd, directTreeFile);
 
+	int versionNumber = 0;
+	get_version_number(projectName , &versionNumber);
+
+	int len = floor(log10(versionNumber)) + 1;
+	char* versionStr = malloc(len + 1);
+	sprintf(versionStr, "%d",  versionNumber);
+	sendTo(fd, versionStr);
+
+	char* manifestName = generate_manifest_name(projectName, versionNumber);
+	send_file(fd, manifestName);
+
+	//2
+	int i;
+	int sizeManifest = 0;
+	manEntry** manifestEntry = readManifest(manifestName, &sizeManifest);
+
+	printf("manifestName = %s, sizeManifest %d\n",manifestName,sizeManifest);
+	for(i = 0; i < sizeManifest; i++)
+	{
+		if(strstr(manifestEntry[i]->name, ".manifest") == 0)
+		{
+			send_file(fd, manifestEntry[i]->name);
+		}
+	}
 
     free(projectName);
-
+    free(directTreeFile);
+    free(versionStr);
+    free(manifestName);
+    freeManifest(manifestEntry, &sizeManifest);
 
 }
